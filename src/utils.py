77120a85bf09d0aa433922cfe343ce43048ad57e
kedarmuley91 <kedarmuley91@gmail.com>
2026-01-06 20:25:00 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -29,27 +29,23 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for i in range(len(list(models))):
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
             model = list(models.values())[i]
 
-           
-            para = param.get(list(models.keys())[i], {})
+            para = param.get(model_name, {})
 
             if para:
                 gs = GridSearchCV(model, para, cv=3)
                 gs.fit(X_train, y_train)
-
                 model.set_params(**gs.best_params_)
 
             model.fit(X_train, y_train)
 
-            y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
-
-            train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
