48e270ee8fa90302b748503a29a9275c0a582fbe
kedarmuley91 <kedarmuley91@gmail.com>
2026-01-07 18:05:26 +0530

    predication Pipline

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import dill
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV   
- 
+
 from src.exception import CustomException
 from src.logger import logging
 
@@ -48,6 +48,15 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
             report[model_name] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):   # ← FIXED (removed extra space)
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
